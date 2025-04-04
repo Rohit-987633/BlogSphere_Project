8c7ced4e876b1b72c3cc11297e513ec80d26435b
root <987633@win.edu.au>
2025-04-04 15:31:44 +1100

    Home page design semi-final

--- Home/index.php
+++ Home/index.php
@@ -41,11 +41,52 @@ background-repeat: no-repeat;
 
 
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200"><path fill="#fff" fill-opacity="1" d="M0,160L21.8,154.7C43.6,149,87,139,131,122.7C174.5,107,218,85,262,101.3C305.5,117,349,171,393,176C436.4,181,480,139,524,133.3C567.3,128,611,160,655,165.3C698.2,171,742,149,785,122.7C829.1,96,873,64,916,53.3C960,43,1004,53,1047,85.3C1090.9,117,1135,171,1178,181.3C1221.8,192,1265,160,1309,144C1352.7,128,1396,128,1418,128L1440,128L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
+            <path fill="#fff" fill-opacity="1" d="M0,160L21.8,154.7C43.6,149,87,139,131,122.7C174.5,107,218,85,262,101.3C305.5,117,349,171,393,176C436.4,181,480,139,524,133.3C567.3,128,611,160,655,165.3C698.2,171,742,149,785,122.7C829.1,96,873,64,916,53.3C960,43,1004,53,1047,85.3C1090.9,117,1135,171,1178,181.3C1221.8,192,1265,160,1309,144C1352.7,128,1396,128,1418,128L1440,128L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path>
+        </svg>
     </div>
+
     <div class="container">
         <div class="main_section_part">
-            <h1 style="text-align: center;padding:30px;">Meet Our Team</h1>
+            <h1 style="text-align: center;padding:30px;">TOP 3 BLOG </h1>
+
+            <div class="first_blog">
+                <div class="home_left_part_blog">
+                    <img src="../assets///blog4.jpg" alt="">
+                </div>
+                <div class="home_right_part_blog">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil autem officia id officiis corporis nostrum, eaque architecto eum, quas doloremque cumque inventore, in aut sequi incidunt culpa repudiandae repellat debitis commodi illum blanditiis minima. Officia ab voluptatibus quos est autem nam necessitatibus magnam id facilis laboriosam, illum nisi vitae, soluta quod inventore dolore? Ducimus et beatae veritatis harum perferendis. Sunt sapiente obcaecati minima. In delectus aperiam illum vitae. Fugiat, suscipit.</h2>
+                </div>
+            </div>
+
+
+            <div class="first_blog">
+                <div class="home_left_part_blog">
+                    <img src="../assets///blog2.jpg" alt="">
+                </div>
+                <div class="home_right_part_blog">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil autem officia id officiis corporis nostrum, eaque architecto eum, quas doloremque cumque inventore, in aut sequi incidunt culpa repudiandae repellat debitis commodi illum blanditiis minima. Officia ab voluptatibus quos est autem nam necessitatibus magnam id facilis laboriosam, illum nisi vitae, soluta quod inventore dolore? Ducimus et beatae veritatis harum perferendis. Sunt sapiente obcaecati minima. In delectus aperiam illum vitae. Fugiat, suscipit.</h2>
+                </div>
+            </div>
+
+
+
+            <div class="first_blog">
+                <div class="home_left_part_blog">
+                    <img src="../assets///blog3.jpg" alt="">
+                </div>
+                <div class="home_right_part_blog">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil autem officia id officiis corporis nostrum, eaque architecto eum, quas doloremque cumque inventore, in aut sequi incidunt culpa repudiandae repellat debitis commodi illum blanditiis minima. Officia ab voluptatibus quos est autem nam necessitatibus magnam id facilis laboriosam, illum nisi vitae, soluta quod inventore dolore? Ducimus et beatae veritatis harum perferendis. Sunt sapiente obcaecati minima. In delectus aperiam illum vitae. Fugiat, suscipit.</h2>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="main_section_part">
+            <h1 style="text-align: center;padding:30px;">TESTIMONIALS</h1>
             <div class="parent">
                 <div class="div1">
                     <div class="card_header">
@@ -63,7 +104,7 @@ background-repeat: no-repeat;
                         <img src="../assets//logo.png" alt="">
                     </div>
                     <div class="card_body">
-                    <h1>Ankita Pokhrel</h1> <br>
+                        <h1>Ankita Pokhrel</h1> <br>
                         <p>Lorem ipsum dolor sit amet . Inventore maxime, ex autem totam sequi sint recusandae veniam excepturi animi? Quod voluptates corrupti minima velit porro accusamus, numquam deleniti ducimus eius et repellendus nulla, a ad cum labore soluta id est?
 
                         </p>
@@ -74,7 +115,7 @@ background-repeat: no-repeat;
                         <img src="../assets//logo.png" alt="">
                     </div>
                     <div class="card_body">
-                    <h1>Bhimsen Parajuli </h1> <br>
+                        <h1>Bhimsen Parajuli </h1> <br>
                         <p>Lorem ipsum dolor sit amet . Inventore maxime, ex autem totam sequi sint recusandae veniam excepturi animi? Quod voluptates corrupti minima velit porro accusamus, numquam deleniti ducimus eius et repellendus nulla, a ad cum labore soluta id est?
 
                         </p>
@@ -83,6 +124,21 @@ background-repeat: no-repeat;
             </div>
         </div>
     </div>
+    <footer class="foot_home">
+        <di v class="container">
+            <div class="subscribe_part">
+                <input type="email" name="" id="">
+                <button class="subscribe_part_btn">Submit</button>
+            </div>
+            <div class="social_media">
+                <img class="social_media_img" src="../assets//logo.png" alt="">
+                <img class="social_media_img" src="../assets//logo.png" alt="">
+                <img class="social_media_img" src="../assets//logo.png" alt="">
+                <img class="social_media_img" src="../assets//logo.png" alt="">
+            </div>
+            <p style="text-align: center;color:#6d6969;padding:20px ">Copyright © 2025 Nepal Tech. All Rights Reserved.</p>
+            </div>
+    </footer>
 </body>
 
 </html>
